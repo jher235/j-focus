dc594e6820d8debafda434760d6f874e16920a46
jher235 <tim668666@gmail.com>
2026-01-26 03:23:08 +0900

    chore: setup build configuration with Shadow plugin and essential dependencies - com.github.johnrengelman.shadow: 컴파일 시 라이브러리들까지 모두 실행 파일로 만들기 위함 - AST 분석을 위해 JavaParser 의존성 추가 - cli 입력을 위해 Picocli 의존성 추가 - 메인 함수는 com.jher235.jfocus.JFocusCli 로 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "com.jher235"
@@ -11,10 +12,23 @@ repositories {
 
 dependencies {
 
+    implementation("com.github.javaparser:javaparser-symbol-solver-core:3.25.10")
+
+    implementation("info.picocli:picocli:4.7.5")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.5")
+
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "com.jher235.jfocus.JFocusCli"
+    }
+}
+
 
 tasks.test {
     useJUnitPlatform()
