4542a2b4cd4233efdc9e8241194fcf74fa9bd73f
jher235 <tim668666@gmail.com>
2026-01-26 18:48:18 +0900

    chore: upgrade dependency versions and specify jdk version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "8.3.6"
 }
 
 group = "com.jher235"
@@ -10,19 +10,31 @@ repositories {
     mavenCentral()
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
 dependencies {
 
-    implementation("com.github.javaparser:javaparser-symbol-solver-core:3.25.10")
+    // Java Parser
+    implementation("com.github.javaparser:javaparser-symbol-solver-core:3.26.2")
 
-    implementation("info.picocli:picocli:4.7.5")
-    annotationProcessor("info.picocli:picocli-codegen:4.7.5")
+    // CLI
+    implementation("info.picocli:picocli:4.7.6")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.6")
 
-    implementation("org.eclipse.jgit:org.eclipse.jgit:6.8.0.202311291450-r")
+    // JGit
+    implementation("org.eclipse.jgit:org.eclipse.jgit:7.1.0.202411261347-r")
 
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    // Test
+    testImplementation(platform("org.junit:junit-bom:5.11.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    testImplementation("org.assertj:assertj-core:3.26.3")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.assertj:assertj-core:3.25.3")
 }
 
 tasks.jar {
