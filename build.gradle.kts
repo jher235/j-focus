b3fdba9257444a9ce2b1d9cd570219f6ff562f62
jher235 <tim668666@gmail.com>
2026-01-26 18:27:16 +0900

    chore: add dependency about jgit & assertJ

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,10 +17,12 @@ dependencies {
     implementation("info.picocli:picocli:4.7.5")
     annotationProcessor("info.picocli:picocli-codegen:4.7.5")
 
+    implementation("org.eclipse.jgit:org.eclipse.jgit:6.8.0.202311291450-r")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.assertj:assertj-core:3.25.3")
 }
 
 tasks.jar {
